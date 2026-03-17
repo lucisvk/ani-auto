6c010c5fdf5bc4bc4f176c7dd99107de4438fd0c
Tyler Morency <Tylermlucis@gmail.com>
2026-03-17 16:09:27 -0400

    Initial commit. Added the ablity for script to enter in the desired anime to watch as well as a session log.

---
+++ ani-cliscript.py
@@ -0,0 +1,42 @@
+import pexpect
+import sys
+
+log_file = open("session_log.txt", "w", encoding="utf-8")
+
+class Logger:
+    def write(self, data):
+        sys.stdout.write(data)
+        log_file.write(data)
+
+    def flush(self):
+        sys.stdout.flush()
+        log_file.flush()
+
+print("Welcome to the auto downloader")
+
+child = pexpect.spawn("ani-cli", encoding="utf-8")
+child.logfile = Logger()
+
+userin = input("Enter what show you would like to download: ")
+
+child.expect("Search", timeout=10)
+child.sendline(userin)
+
+child.expect("Select", timeout=10)
+child.sendline("1")
+child.sendline("\r")
+child.sendline("")
+
+
+#episode = 69
+#print(f"{userin} has {episode}")
+
+#user_range_start = int(input("Enter the episode to start at: "))
+#user_range_end = int(input("Enter the episode to end at: "))
+
+child.expect("Select episode", timeout=10)
+child.sendline(str(9))
+
+child.expect(pexpect.EOF)
+
+print("Done")
